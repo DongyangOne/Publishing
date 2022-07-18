83e9962f14d98c9de3aaa3989a019b0d95dd607e
dhwlghks <ojhukes@gmail.com>
2022-07-18 20:43:41 +0900

    오지환 Login,Register 컴포넌트
    - Login_btn.jsx + Login_btn.scss
    
    - Register_btn.jsx + Register_btn.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import '../styles/App.scss'
+import Login_btn from './pages/Login_btn'
+import Register_btn from './pages/Register_btn'
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello </h1>
-    </div>
+  <div>
+  <Login_btn/>
+  <Register_btn/>
+  </div>
+  
   )
+  
 }
---
+++ src/pages/Register_btn.jsx
@@ -0,0 +1,36 @@
+import '../../styles/Register_btn.scss'
+
+export default function Register_btn () {
+    return (
+        <div>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@700&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="index.css" />    
+        <title>Document</title>
+        <div className="bluebox"><p className="bluebox_anyad">ANYAD</p></div>
+        <div className="whitebox">
+          <div className="wrap">
+            <p>ANYAD에 오신걸 환영합니다!</p>
+            <p className="ex">소셜 디자인으로 ANYAD에 참여해보세요.</p>
+            <form action>
+              <button type="submit" className="google">GOOGLE 계정으로 회원가입</button>
+              <br />
+              <button type="submit" className="kakao">KAKAO 계정으로 회원가입</button>
+              <div className="line1" />
+              <p className="or">또는</p>
+              <div className="line2" />
+              <button type="submit" className="anyad">ANYAD 회원가입</button>
+            </form>
+          </div>
+        </div>
+      </div>
+
+    )
+}
